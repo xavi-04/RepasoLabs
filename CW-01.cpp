de6f149aaaed7710aa10162d4c8180c9c96abb48
KennetQ <00114718@uca.edu.sv>
2020-01-14 12:28:32 -0600

    Add: CW01-CW05

---
+++ CW-01.cpp
@@ -0,0 +1,18 @@
+//Realiza un programa que almacene las notas de 20 personas asistentes a los laboratorios
+#include <iostream>
+using namespace std;
+
+int main(void){
+float array[20];
+
+for(int i = 0; i < 20; i++){
+cout << " Digite la nota: "; cin >> array[i];
+}
+
+for(int = 0; i < 20; i++){
+    cout << "Nota " << (i + 1) << ":\t";
+}
+
+
+    return 0;
+}
